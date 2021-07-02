3541ca2824304f0bb878384e0dd7fc57ff3f2a58
apoorva srivastava <apoorvasrivastava.14@gmail.com>
2021-07-02 20:45:53 +0530

    added codechef july challenge questions

---
+++ Codechef/July Long challenge/OptimalDenomination.cpp
@@ -0,0 +1,29 @@
+// You are the owner of a big company. You are so rich, that the government has allowed you to print as many notes as you want of any single value that you like. You also have peculiar behavioral traits and you often do things that look weird to a third person.
+
+// You have N employees, where the i-th employee has salary Ai. You want to pay them using a denomination that you create. You are also eco-friendly and wish to save paper. So, you wish to pay them using as few notes as possible. Find out the minimum number of notes required if you can alter the salary of at most one employee to any positive integer that you like, and choose the positive integer value that each note is worth (called its denomination).
+
+// Each employee must receive the exact value of his/her salary and no more.
+
+// Input
+// The first line contains an integer T, the number of test cases. Then the test cases follow.
+// The first line of each test case contains a single integer N.
+// The second line contains N integers A1,A2,…,AN, where Ai is the salary of the i-th employee.
+// Output
+// For each test case, output in a single line the answer to the problem.
+
+// Constraints
+// 1≤T≤12⋅104
+// 1≤N≤105
+// 1≤Ai≤109
+// The sum of N over all test cases is at most 106.
+#include <bits/stdc++.h>
+using namespace ::std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+    }
+    return 0;
+}
