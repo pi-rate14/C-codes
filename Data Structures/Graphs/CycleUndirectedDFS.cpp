146478c72948474d083879919bb9c93a7afef252
pi-rate14 <apoorvasrivastava.14@gmail.com>
2022-11-09 19:44:44 +0530

    added graph problems

---
+++ Data Structures/Graphs/CycleUndirectedDFS.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+
+private:
+    bool detect(int src, int parent, vector<bool> &vis, vector<int> adj[])
+    {
+
+        vis[src] = true;
+        for (auto adjNode : adj[src])
+        {
+            if (!vis[adjNode])
+            {
+                if (detect(adjNode, src, vis, adj))
+                {
+                    return true;
+                }
+            }
+            else if (adjNode != parent)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+public:
+    // Function to detect cycle in an undirected graph.
+    bool
+    isCycle(int V, vector<int> adj[])
+    {
+        // Code here
+        vector<bool> vis(V, false);
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                if (detect(i, -1, vis, adj))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++)
+        {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
